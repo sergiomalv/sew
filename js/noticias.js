19f22fefdd406a02d91a6824ed905084f5c2881e
sergiomalv <sermual@gmail.com>
2023-12-11 20:03:01 +0100

    Update input labels and ids for noticias.js and noticias.html

--- js/noticias.js
+++ js/noticias.js
@@ -4,8 +4,8 @@ class Noticias {
 
         if (window.File && window.FileReader && window.FileList && window.Blob) {
             const main = $("main");
-            const label = $("<label>Selecciona un archivo de texto: </label>");
-            const input = $("<input type='file' name='files' onchange='noticias.readInputFile(this.files);'>");
+            const label = $("<label for='files'>Selecciona un fichero de texto:</label>");
+            const input = $("<input type='file' name='files' id='files' onchange='noticias.readInputFile(this.files);'>");
             main.append(label).append(input);
         }
         else document.write("<p>¡¡¡ Este navegador NO soporta el API File y este programa puede no funcionar correctamente !!!</p>");
